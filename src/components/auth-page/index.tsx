a8dd2340f1ca4205ac078668493c88d821f2e91c
Daniil <you@example.com>
2024-10-31 18:21:37 +0300

    fix: 🐛 test env

--- src/components/auth-page/index.tsx
+++ src/components/auth-page/index.tsx
@@ -3,6 +3,12 @@ import { AuthPage as AuthPageBase } from "@refinedev/antd";
 import type { AuthPageProps } from "@refinedev/core";
 
 export const AuthPage = (props: AuthPageProps) => {
+  console.log("VERCEL_URL", process.env.VERCEL_URL);
+  console.log("VERCEL_BRANCH_URL", process.env.VERCEL_BRANCH_URL);
+  console.log(
+    "VERCEL_PROJECT_PRODUCTION_URL",
+    process.env.VERCEL_PROJECT_PRODUCTION_URL
+  );
   return (
     <AuthPageBase
       {...props}
